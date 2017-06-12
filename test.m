a14ddf25d6c5f99609893d07b0e6de52e97c4ff6
jlalle19 <jax.9.allen@gmail.com>
2017-06-11 18:44:27 -0700

    Update

--- test.m
+++ test.m
@@ -1,6 +1,15 @@
 %%TESTING
+clear; clc; close all;
+%load('TestChannels.mat');
+fileID = fopen('TestChannels.bin','r');
 
-x = 2
-y = 2
-x + y
-x
+%StackOverflow
+NAME_TYPE = 'int16';
+SIZE_TYPE = 2;  %Bytes per int
+fseek(fileID, 0, 'eof');
+sizeA  = fix(ftell(fileID) / SIZE_TYPE)/64;
+fseek(fileID, 0, 'bof');
+file = fread(fileID, [sizeA,64], NAME_TYPE)
+
+
+fclose(fileID);
