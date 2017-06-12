8383fd4017434f46a5812b7338f21729d28f09b5
jlalle19 <jax.9.allen@gmail.com>
2017-06-11 20:12:12 -0700

    Update
    Added scaling and file selection.

--- test.m
+++ test.m
@@ -1,15 +1,23 @@
 %%TESTING
 clear; clc; close all;
-%load('TestChannels.mat');
-fileID = fopen('TestChannels.bin','r');
 
-%StackOverflow
+%Importing file to a sizeA x 64 array
 NAME_TYPE = 'int16';
 SIZE_TYPE = 2;  %Bytes per int
+[FileName,PathName] = uigetfile('*.bin','Select a data file');
+fileID = fopen(FileName,'r');
+if(fileID < 0)
+    error('File not found, please place binary file in repository.')
+end
 fseek(fileID, 0, 'eof');
 sizeA  = fix(ftell(fileID) / SIZE_TYPE)/64;
 fseek(fileID, 0, 'bof');
-file = fread(fileID, [sizeA,64], NAME_TYPE)
+file = fread(fileID, [sizeA,64], NAME_TYPE);
+
+%%Scaleing file to scaleing factor SCALE_FACTOR
+SCALE_FACTOR = 3.05185094759972*10^(-05); %Conversion factor from short to mV
+file = double(file)*SCALE_FACTOR;
+
 
 
 fclose(fileID);
